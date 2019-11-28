b14a5b7cd728540e87c2fb400324ae432db614c9
Guillermo Mora <guillermo@cleverppc.com>
2019-11-28 12:46:51 +0100

    Finish registration. Fix database

---
+++ auth/app/controllers/registrations_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class RegistrationsController < ApplicationController
+  def new; end
+
+  def create
+    filtered_params = params.require(:registration).permit(:username)
+    user = User.new(username: filtered_params[:registration][:username])
+
+    create_options = WebAuthn::Credential.options_for_create(
+      user: {
+        name: user.username,
+        id: user.webauthn_id
+      }
+    )
+
+    if user.valid? # Do not save, but check if it could be saved
+      data = { status: 'ok', challenge: create_options.challenge, user_attributes: user.attributes }
+      render json: data
+    else
+      render json: { status: 'error', message: 'User already exists' }, status: :unprocessable_entity
+    end
+  end
+
+  def callback
+    filtered_params = params.require(:registration).permit(:user, :challenge, :publicKeyCredential)
+    webauthn_credential = WebAuthn::Credential.from_create(params[:registration][:publicKeyCredential])
+
+    user = User.create!(params[:registration][:user])
+    webauthn_credential.verify(filtered_params[:registration][:challenge])
+
+    key = user.keys.build(
+      external_id: Base64.strict_encode64(webauthn_credential.raw_id),
+      public_key: webauthn_credential.public_key,
+      sign_count: webauthn_credential.sign_count
+    )
+
+    if key.save
+      render json: { status: 'ok' }, status: :ok
+    else
+      render json: { status: 'error', message: 'Key not registered' },
+             status: :unprocessable_entity
+    end
+  rescue WebAuthn::Error => e
+    render json: { status: 'error', message: "Verification failed: #{e.message}" },
+           status: :unprocessable_entity
+  rescue StandardError => e
+    render json: { status: 'error', message: 'User could not be created' },
+           status: :unprocessable_entity
+  end
+end
--- auth/db/migrate/20191128094449_init_database.rb
+++ auth/db/migrate/20191128094449_init_database.rb
@@ -12,7 +12,6 @@ class InitDatabase < ActiveRecord::Migration[6.0]
 
     create_table :keys do |t|
       t.string :external_id
-      t.string :name
       t.string :public_key
       t.integer :login_count, default: 0, null: false
       t.belongs_to :user, index: true
