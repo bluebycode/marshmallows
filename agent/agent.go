82b2050270bbe74dd760385a83c9b01dc388e69b
vrandkode <vrandkode@gmail.com>
2019-11-28 16:30:47 +0100

    agent. add websocket client endpoint + healthcheck

--- agent/agent.go
+++ agent/agent.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
+func main() {
+	token := GenerateToken(6)
+	fmt.Println("[agent]token", token)
+
+	// authorisation channel
+	shutdown := make(chan bool)
+	done := make(chan struct{})
+	conn := authConnection("localhost", 8081, "/open/"+token) // @todo: ride off placeholder
+	defer conn.Close()
+	go func() {
+		defer close(done)
+		authHandler(token, conn, shutdown)
+	}()
+
+	// builds an interval signal used as Hearbeat
+	healthcheck := time.NewTicker(10 * time.Second)
+	defer healthcheck.Stop()
+
+	for {
+		select {
+		case <-shutdown:
+			log.Println("[agent] closing...")
+			return
+		case timestamp := <-healthcheck.C:
+			healthcheckHandler(conn, timestamp)
+		}
+	}
 }
