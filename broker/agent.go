786c1ad5a6b43e1e686d4c430732d8064ee73e9f
vrandkode <vrandkode@gmail.com>
2019-11-28 14:08:07 +0100

    Broker. Main functionality which includes peers representation and main message structures

---
+++ broker/agent.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+type Agent struct {
+	*Peer
+	token     string
+	timestamp int64
+}
+
+func (agent *Agent) close() {
+	// Closing the peer
+}
+
+func (agent *Agent) read() {
+	defer agent.close()
+
+	for {
+		messageType, p, err := agent.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("[peer::agent] Error: %v", err)
+			}
+			break
+		}
+
+		// Reading the message from peer
+		// ...
+		log.Println("[peer::agent] Reading", messageType, p)
+		msg := &Message{} // replace this
+
+		select {
+		case agent.hub.agentsIncoming <- msg:
+			// Forward message to hub dedicated topic
+			// ...
+			log.Println("[peer::agent] Incoming message to hub", msg)
+		}
+	}
+}
