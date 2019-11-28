5dc1365631cc9d372a05ea8d21a9035f113f86a8
vrandkode <vrandkode@gmail.com>
2019-11-28 16:29:32 +0100

    broker. websockets handlers. auxiliar functions and patterns [developed before competition]

--- broker/websockets.go
+++ broker/websockets.go
@@ -1,9 +1,32 @@
 package main
 
-import "github.com/gorilla/websocket"
+// @vrandkode
+// Developed before competition.
+// This file contains handlers and patterns functionality helpers with Websockets.
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
 
 // Buffer definitions
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+}
+
+func handleConnection(connection *websocket.Conn, messageType int, packet []byte) {
+	// ...
+}
+
+func wsHandler(connection *websocket.Conn, shutdown chan bool) {
+	for {
+		messageType, data, err := connection.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		handleConnection(connection, messageType, data)
+	}
 }
