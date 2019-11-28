6c6a26b17a24900f5ceab2599b58ade993ab734e
vrandkode <vrandkode@gmail.com>
2019-11-29 00:34:01 +0100

    Broker. Adding real frontend side websocket channel -> it should redirect I/O per channel and bridge over secure channel

--- broker/channels.go
+++ broker/channels.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	_ "websocket"
 
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
+
+type channelIO struct {
+	sid      int
+	cin      chan []byte
+	cout     chan []byte
+	incoming chan []byte
+	outgoing chan []byte
+	release  chan bool
+}
+
+func newChannelIO(sid int) *channelIO {
+	return &channelIO{
+		sid:      sid,
+		cin:      make(chan []byte, 1),
+		cout:     make(chan []byte, 1),
+		incoming: make(chan []byte, 1),
+		outgoing: make(chan []byte, 1),
+		release:  make(chan bool, 1)}
+}
+
+var sources = make(map[string]int)
 
 // createChannel ... create channel and attach I/O on channel
 func createChannel(channelID string, port int, finished chan bool,
@@ -15,13 +38,13 @@ func createChannel(channelID string, port int, finished chan bool,
 	log.Println("Creating channel ", channelID, "port:", port)
 
 	channelMux := http.NewServeMux()
-	channelMux.HandleFunc("/ws", wsChannelHandler(channelID, incoming, outgoing, cin, cout))
+	channelMux.HandleFunc("/ws", wsAdminChannelHandler(channelID, incoming, outgoing, cin, cout))
 	go createServerChannel("localhost", port, channelMux) //@todo: add security and non placeholders addresses
 	finished <- true
 }
 
-// wsChannelHandler ... receives and handle incoming websockets connections, assign them into channel hub
-func wsChannelHandler(channelID string, incoming *chan []byte, outgoing *chan []byte, cin *chan []byte, cout *chan []byte) func(w http.ResponseWriter, r *http.Request) {
+// wsAdminChannelHandler ... receives and handle incoming websockets connections, assign them into channel hub
+func wsAdminChannelHandler(channelID string, incoming *chan []byte, outgoing *chan []byte, cin *chan []byte, cout *chan []byte) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 		ws, err := upgrader.Upgrade(w, r, nil)
@@ -30,11 +53,11 @@ func wsChannelHandler(channelID string, incoming *chan []byte, outgoing *chan []
 			return
 		}
 		h := newChannelHub(*incoming, *outgoing, *cin, *cout)
-		handlerSecureChannel(channelID, ws, h)
+		handlerAdminSecureChannel(channelID, ws, h)
 	}
 }
 
-func handlerSecureChannel(channelID string, c *websocket.Conn, h *channelHub) {
+func handlerAdminSecureChannel(channelID string, c *websocket.Conn, h *channelHub) {
 	go h.run()
 	h.newConnection <- c
 	for {
@@ -45,5 +68,46 @@ func handlerSecureChannel(channelID string, c *websocket.Conn, h *channelHub) {
 		}
 		log.Println("[channel] message:", message)
 		h.outgoing <- message
+	}
+}
+
+func wsChannelHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	channelID := params["channel"]
+
+	//@todo replace with a real source using X-Forward-IP ?¿
+	source := "127.0.0.1"
+
+	// check upgrader
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("upgrade:", err)
+		return
+	}
+	fmt.Println("session:", channelID)
+
+	// create pipeline attached to session
+	sid := sources[source]
+	c := newChannelIO(sid)
+	channels[sid] = *c
+
+	// create common hub
+	fmt.Println("channels:", channels, *c)
+	hub := newChannelHub(c.incoming, c.outgoing, c.cin, c.cout)
+	wsChannelHubHandler(ws, hub) // waiting for messages
+	fmt.Println("pass!")
+}
+
+func wsChannelHubHandler(c *websocket.Conn, h *channelHub) {
+	go h.runCommon()
+
+	fmt.Println("[hub] waiting for connections")
+	h.newConnection <- c
+
+	for {
+		messageType, message, _ := c.ReadMessage()
+		fmt.Println("[hub] read...'", message, string(message), messageType)
+		h.commonOutgoing <- message
 	}
 }
