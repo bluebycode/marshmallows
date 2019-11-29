c54bd7f304d07902f138227e9c95e65813e77ff1
vrandkode <vrandkode@gmail.com>
2019-11-29 17:17:22 +0100

    agent-broker. flow authentication. agent sends the AuthToken{}

---
+++ poc/httpnoise/client/client.go
@@ -0,0 +1,86 @@
+package main
+
+// References: https://raw.githubusercontent.com/go-noisesocket/noisesocket
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+
+	requests "./protocol"
+	"github.com/flynn/noise"
+	"github.com/golang/protobuf/proto"
+	"gopkg.in/noisesocket.v0"
+)
+
+func httpClient(address string, port int, path string, buffer []byte) {
+	pub1, _ := base64.StdEncoding.DecodeString("L9Xm5qy17ZZ6rBMd1Dsn5iZOyS7vUVhYK+zby1nJPEE=")
+	priv1, _ := base64.StdEncoding.DecodeString("TPmwb3vTEgrA3oq6PoGEzH5hT91IDXGC9qEMc8ksRiw=")
+
+	clientKeys := noise.DHKey{
+		Public:  pub1,
+		Private: priv1,
+	}
+
+	transport := &http.Transport{
+		MaxIdleConnsPerHost: 1,
+		DisableKeepAlives:   true,
+
+		DialTLS: func(network, addr string) (net.Conn, error) {
+			conn, err := noisesocket.Dial(addr, &noisesocket.ConnectionConfig{StaticKey: clientKeys})
+			if err != nil {
+				fmt.Println("Dial", err)
+			}
+			return conn, err
+		},
+	}
+
+	c := make(chan bool)
+	go func() {
+		cli := &http.Client{
+			Transport: transport,
+		}
+		reader := bytes.NewReader(buffer)
+		req, err := http.NewRequest("POST", "https://"+address+":"+strconv.Itoa(port)+path, reader)
+		if err != nil {
+			panic(err)
+		}
+
+		resp, err := cli.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		_, err = io.Copy(ioutil.Discard, resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		err = resp.Body.Close()
+		if err != nil {
+			panic(err)
+		}
+		c <- true
+	}()
+
+	<-c
+
+	fmt.Println("done.")
+}
+
+func main() {
+	auth := &requests.AgentAuth{
+		Token:     proto.String("mytokenmaravilloso"),
+		PublicKey: proto.String("mysecretpublickey"),
+		DeviceId:  proto.String("abcde"),
+	}
+	data, err := proto.Marshal(auth)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	httpClient("localhost", 7777, "/open", data)
+}
