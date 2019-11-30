8b3d76975ceb4ebc4a8f2ea8ec6d4649ac58d9bd
vrandkode <vrandkode@gmail.com>
2019-11-30 04:33:15 +0100

    agent

--- agent/agent.go
+++ agent/agent.go
@@ -2,51 +2,33 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
-	"websocket"
-
-	"github.com/kr/pty"
+	"time"
 )
 
-func pttyAttach(c *websocket.Conn) {
-	cmd := exec.Command("bash")
-	f, _ := pty.Start(cmd)
-	wc := &rwc{c: c}
-	p := &Pipe{}
-	go p.in(f, wc)
-	p.out(wc, f)
-}
+var brokerAddress = "localhost:8082"
 
 func main() {
-	token := GenerateToken(6)
-	fmt.Println("[agent]token", token)
 
-	createClientPlainChannel("localhost", 9999, "/ws", func(in []byte, size int) []byte { return in },
-		func(conn *websocket.Conn) {
-			pttyAttach(conn)
-		})
-	/*
-		// authorisation channel
-		shutdown := make(chan bool)
-		done := make(chan struct{})
-		conn := authConnection("localhost", 8081, "/open/"+token) // @todo: ride off placeholder
-		defer conn.Close()
-		go func() {
-			defer close(done)
-			authHandler(token, conn, shutdown)
-		}()
+	// Agent identification
+	id := generateId()
 
-		// builds an interval signal used as Hearbeat
-		healthcheck := time.NewTicker(10 * time.Second)
-		defer healthcheck.Stop()
+	// Ask for secret token provided with the distribution
+	secretToken := readToken()
 
-		for {
-			select {
-			case <-shutdown:
-				log.Println("[agent] closing...")
-				return
-			case timestamp := <-healthcheck.C:
-				healthcheckHandler(conn, timestamp)
-			}
-		}*/
+	// {D,d} keys used to shared with broker sharing with the party
+	keys := generateKeys()
+	sendAuthentication(brokerAddress+"/open", secretToken, &keys, id, func([]byte) {})
+
+	fmt.Println("token", secretToken)
+
+	// builds an interval signal used as Hearbeat
+	beacon := time.NewTicker(30 * time.Second)
+	defer beacon.Stop()
+
+	for {
+		select {
+		case timestamp := <-beacon.C:
+			handleBeaconRequest(&keys, id, timestamp)
+		}
+	}
 }
